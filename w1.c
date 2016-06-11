a51bef10a3a5059647163295cb5766b2c62201f0
mustafabastaki <mbastaki@myseneca.ca>
2016-06-11 18:45:39 +0000

    Add files via upload

---
+++ w1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("+--------------+\n");
+	printf("|              |\n");
+	printf("|  CODE QUEST  |\n");
+	printf("|              |\n");
+	printf("+--------------+\n");
+
+	return 0;}
+
